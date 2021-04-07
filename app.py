5e05d12c3eb06658f3bf1cf40165065ace5fa2c8
Zak Pruitt <pruittzn@gmail.com>
2021-04-07 05:47:11 -0500

    init

--- app.py
+++ app.py
@@ -1,291 +1,34 @@
-from flask import Flask, redirect, url_for, render_template, request, session, g, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+"""aaa."""
 import os
-import pymongo
-from pymongo import MongoClient
+from flask import Flask
+from routes.init import init
+from routes.user import user
+from routes.assignment import assignment
 
 app = Flask(__name__)
-app.secret_key = os.environ.get('SECRET')
+app.register_blueprint(init)
+app.register_blueprint(user)
+app.register_blueprint(assignment)
 
-cluster = pymongo.MongoClient(os.environ.get('MONGODB_URI'))
-db = cluster["wow-data"]
-user_collection = db["user-data"]
-dungeon_collection = db["dungeon-data"]
-
-
-@app.before_request
-def before_request():
-    if 'username' in session:
-        user = user_collection.find_one({"username": session["username"]})
-        g.user = user
-    else:
-        pass
-
-
-@app.route("/", methods=["GET"])
-def home():
-    return render_template("index.html")
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session.pop("username", None)
-        username = request.form["username"]
-        password = request.form["password"]
-
-        user = user_collection.find_one({"username": username})
-        if user and check_password_hash(user["password"], password):
-            session["username"] = user["username"]
-            return redirect("/")
-        else:
-            flash("Incorrect username or password.")
-            return redirect("/login")
-    else:
-        return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        user = user_collection.find_one({"username": username})
-        if user:
-            flash("Unfortunately, that username is already taken. Please try another.")
-            return redirect("/register")
-
-        password = request.form["password"]
-        cpassword = request.form["cpassword"]
-        if password != cpassword:
-            flash("Your passwords did not match.")
-            return redirect("/register")
-
-        spec = request.form["spec"]
-        hashed_password = generate_password_hash(password, method="sha256")
-
-        post = {
-            "username": username,
-            "password": hashed_password,
-            "spec": spec,
-            "necrotic_wake": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "plaguefall": {
-               "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "tirna_scithe": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "atonement": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "theatre": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "de_other_side": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "spires": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            },
-            "sanguine_depths": {
-                "plus0T": 0,
-                "plus1T": 0,
-                "plus2T": 0,
-                "plus3T": 0,
-                "plus4T": 0,
-                "plus5T": 0,
-                "plus6T": 0,
-                "plus7T": 0,
-                "plus8T": 0,
-                "plus9T": 0,
-                "plus10T": 0,
-                "plus0": 0,
-                "plus1": 0,
-                "plus2": 0,
-                "plus3": 0,
-                "plus4": 0,
-                "plus5": 0,
-                "plus6": 0,
-                "plus7": 0,
-                "plus8": 0,
-                "plus9": 0,
-                "plus10": 0
-            }
-        }
-        user_collection.insert_one(post)
-        flash("Your account was successfully made! Please login now.")
-        return redirect("/login")
-    else:
-        return render_template("register.html")
-
-
-@app.route("/sign-out", methods=["GET"])
-def sign_out():
-    session.pop("username", None)
-    return redirect("/")
-    
-
-@app.route("/dungeon/<abbr>", methods=["GET"])
-def dungeon_page(abbr):
-    if 'username' not in session:
-        flash("Please log in or register to view your stats.")
-        return redirect("/")
-    else:
-        dungeon = dungeon_collection.find_one({"shorthand": abbr})
-
-        if dungeon:
-            return render_template("dungeon.html", dungeon=dungeon,  p_name=dungeon["p_name"])
-        else:
-            return render_template("404.html")
-
+#app.secret_key = os.environ.get('SECRET')
+app.secret_key = "pog"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# region
+# @app.route("/dungeon/<abbr>", methods=["GET"])
+# def dungeon_page(abbr):
+#     """HUH."""
+#     if 'username' not in session:
+#         flash("Please log in or register to view your stats.")
+#         return redirect("/")
+#     else:
+#         dungeon = dungeon_collection.find_one({"shorthand": abbr})
+
+#         if dungeon:
+#             return render_template("dungeon.html", dungeon=dungeon,  p_name=dungeon["p_name"])
+#         else:
+#             return render_template("404.html")
+# endregion
---
+++ routes/init.py
@@ -0,0 +1,28 @@
+"""assas."""
+from flask import Flask, render_template, g, request, session, Blueprint
+from pymongo import MongoClient
+import os
+
+init = Blueprint('init', __name__)
+
+cluster = MongoClient('mongodb+srv://zak:1234@zak-cluster.gp6ka.mongodb.net/wow-data?retryWrites=true&w=majority')
+#cluster = MongoClient(os.environ.get('MONGODB_URI'))
+db = cluster["coin-data"]
+user_collection = db["user"]
+assignment_collection = db["assignment"]
+
+@init.before_app_request
+def find_user():
+    """HUH."""
+    if 'email' in session:
+        user = user_collection.find_one({"email": session["email"]})
+        g.user = user
+    else:
+        pass
+
+
+@init.route("/", methods=["GET"])
+def home():
+    """HUH."""
+    return render_template("index.html")
+
